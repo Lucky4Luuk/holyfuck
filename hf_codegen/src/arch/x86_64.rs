3c729fd078a39e0f77d02c5715446b3bc1d37c51
Luuk van Oijen <lazyluuk.channel@gmail.com>
2024-06-07 00:10:40 +0200

    very basic codegen wip

--- hf_codegen/src/arch/x86_64.rs
+++ hf_codegen/src/arch/x86_64.rs
@@ -4,8 +4,42 @@ use hf_parser::*;
 
 use crate::CodegenError;
 
+fn translate_expr(expr: Expr, a: &mut CodeAssembler) -> Result<(), CodegenError> {
+    match expr {
+        Expr::Add => {
+            a.add(byte_ptr(r8), 0x1).map_err(|e| CodegenError::Generic(format!("{e}")))?;
+        },
+        Expr::Sub => {
+            a.sub(byte_ptr(r8), 0x1).map_err(|e| CodegenError::Generic(format!("{e}")))?;
+        },
+        _ => unimplemented!(),
+    }
+    Ok(())
+}
+
+fn translate_token(token: Token, a: &mut CodeAssembler) -> Result<(), CodegenError> {
+    match token {
+        Token::Expr(expr) => translate_expr(expr, a)?,
+        Token::FuncDecl { name, children } => {
+            // TODO: Implement the function stuff itself
+            for child in children {
+                translate_token(child, a)?;
+            }
+        },
+        _ => unimplemented!(),
+    }
+    Ok(())
+}
+
 pub fn gen_code(addr: u64, modules: Vec<Module>) -> Result<Vec<u8>, CodegenError> {
     let mut a = CodeAssembler::new(64).map_err(|e| CodegenError::Generic(format!("{e}")))?;
+
+    for module in modules {
+        for token in module.tokens {
+            translate_token(token, &mut a)?;
+        }
+    }
+
     a.ret().map_err(|e| CodegenError::Generic(format!("{e}")))?;
 
     let bytes = a.assemble(addr).map_err(|e| CodegenError::Generic(format!("{e}")))?;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let module = hf_parser::parse("main".to_string(), r#"
 :main{
-    +++
+    +
 }"#.to_string()).unwrap();
     // println!("{:#?}", module);
     let machine_code = hf_codegen::gen_code(hf_codegen::Target::X86_64, 0x1234_5678, vec![module]).unwrap();
-    println!("code: {:?}", machine_code);
+    // println!("code: {:?}", machine_code);
+    println!("code:\n{}", machine_code.into_iter().map(|x| format!("0x{:x}", x)).collect::<Vec<String>>().join(", "));
 }
